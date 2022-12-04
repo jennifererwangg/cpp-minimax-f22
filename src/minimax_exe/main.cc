812c3c9ac45c47cb46a4b1042c6fa3308fc87d32
Jennifer <xw2763@columbia.edu>
2022-12-04 18:16:25 -0500

    Update tic tac toe heuristics

--- src/minimax_exe/main.cc
+++ src/minimax_exe/main.cc
@@ -20,12 +20,12 @@ int main(int /*argc*/, char **/*argv*/)
   // cout << "Welcome to tic tac toe player!\n";
   // Automatically play tic tac toe (uncomment to play)
   // std::shared_ptr<GameState> tictactoe = std::make_shared<Tictactoe>(); // create game
-  // AutomaticPlayer tic_tac_toe_player(tictactoe, 10); // create player
+  // AutomaticPlayer tic_tac_toe_player(tictactoe, 5); // create player
   // tic_tac_toe_player.play(); // play game
 
   // Interactive tic tac toe (uncomment to play)
   // std::shared_ptr<Tictactoe> tictactoe = std::make_shared<Tictactoe>(); // create game
-  // InteractiveTictactoePlayer interactive_tic_tac_toe_player(tictactoe, 10); // create player
+  // InteractiveTictactoePlayer interactive_tic_tac_toe_player(tictactoe, 5); // create player
   // interactive_tic_tac_toe_player.play(); // play game
 
   /**
