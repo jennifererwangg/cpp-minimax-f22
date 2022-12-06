bea1f768f1e48976a701a8cafeb686a485fb406a
Shruti Verma <connect.shrutiv@gmail.com>
2022-12-05 23:33:37 -0500

    Added command line arguments to specify automatic or interactive

--- src/games_exe/checkers/main.cc
+++ src/games_exe/checkers/main.cc
@@ -9,18 +9,30 @@
 using namespace std;
 using namespace minimax::core;
 
-int main(int /*argc*/, char **/*argv*/) {
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    cout << "Incorrect usage. Please run: ./checkers <a/i>\n";
+    return 0;
+  }
+
+  string option = argv[1];
+  if (option != "a" && option != "i") {
+    cout << "Incorrect usage. Please run: ./checkers <a/i>\n";
+    return 0;
+  }
   cout << "Welcome to checkers player!\n";
 
-  // Automatically play checkers (uncomment to play)
-  // std::shared_ptr<GameState> checkers = std::make_shared<checkers::Checkers>(); // create game
-  // AutomaticPlayer automatic_checkers(checkers, 5); // create player
-  // automatic_checkers.play(); // play game
-
-  // Interactive tic tac toe (uncomment to play)
-  std::shared_ptr<checkers::Checkers> checkers = std::make_shared<checkers::Checkers>(); // create game
-  InteractiveCheckersPlayer interactive_checkers_player(checkers, 5, "B", "W"); // create player
-  interactive_checkers_player.play(); // play game
+  if (option == "a") {
+    //Automatically play checkers (uncomment to play)
+    std::shared_ptr<GameState> checkers = std::make_shared<checkers::Checkers>(); // create game
+    AutomaticPlayer automatic_checkers(checkers, 5); // create player
+    automatic_checkers.play(); // play game
+  } else {
+    // Interactive tic tac toe (uncomment to play)
+    std::shared_ptr<checkers::Checkers> checkers = std::make_shared<checkers::Checkers>(); // create game
+    InteractiveCheckersPlayer interactive_checkers_player(checkers, 5, "B", "W"); // create player
+    interactive_checkers_player.play(); // play game
+  }
 
   return 0;
 }
