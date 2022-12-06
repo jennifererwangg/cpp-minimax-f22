35c61d5bba5a700e236f87cd7ec612bcb11d46e3
Erin <erin2722@gmail.com>
2022-12-06 16:02:07 -0500

    some docs

--- src/core/games/game_state.h
+++ src/core/games/game_state.h
@@ -16,7 +16,16 @@ enum BoardEntry {
   EMPTY = '-' // for tic tac toe and checkers
 };
 
-// Base class for games
+/**
+ * GameState class
+ * This class is used to represent the state of a game being implemented. It is a base class
+ * with virtual functions, and all game implementations should inherit from it.
+ * 
+ * It holds information about:
+ * The current board state
+ * Whose turn it is
+ * Any additional data required to determine who is winning/what point the game is at
+ */
 class GameState {
 public:
 
@@ -26,13 +35,39 @@ public:
   /**
    * Used by the Minimax Solver
    */
-  // return a vector of all possible next states
+  
+  /**
+   * getNextState()
+   * This function creates the search space for the next iteration of the minimax algorithm.
+   * From the current board state, it should generate all of the next possible moves that 
+   * could be made by the current player.
+   * Eg. for tic tac toe, then we would generate all of the next states by placing an X in 
+   * all of the empty spots of the board.
+   * 
+   * \return A vector of all the possible next game states
+   */
   virtual std::vector<std::shared_ptr<GameState>> getNextState();
 
-  // return the heuristic value of the current state (bigger == more favorable for the player)
+  /**
+   * evalHeuristics()
+   * This function decides how favorable the current board state is for the player. The
+   * higher the number, the more favorable the state is for the current player. This can 
+   * be as simple as (for example, in checkers) counting the number of pieces on the board
+   * for the current player, or can be a complex algorithm evaluating many different factors
+   * for how favorable the current state is for the player.
+   * 
+   * \return an integer representing how favorable the board state is for the current player.
+   */
   virtual int evalHeuristics();
 
-  // return true if the game has reached a terminal state, false otherwise
+  /**
+   * isDone()
+   * This function evaluates whether or not the game is over. It can do this by checking if 
+   * the board is full, if there are no moves left for a player, or some other option indicating
+   * whether or not the game has finished.
+   * 
+   * \return true if the game is over, false if not
+   */
   virtual bool isDone();
 
   /**
@@ -40,17 +75,15 @@ public:
    * 
    */
   
-  // print the current state
+  /**
+   * printState()
+   * Prints the game board along with any other information about the game that should be seen
+   * by the player.
+   */
   virtual void printState();
-
-  // make a move on the board (used by the player)
-  // TODO: don't like that there are 2 different args here, will have to change
-  // virtual bool makeMove(uint row, uint col);
-  // virtual bool makeMove(std::vector<int> initialPlace, std::vector<int> newPlace);
 
   virtual bool makeMove(int y1, int x1, int y2 = 0, int x2 = 0);
 
-  // get the winner of the game
   virtual BoardEntry getWinner();
 
   // set the current player (currently used only by isolation)
